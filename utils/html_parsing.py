ff5b5743bd0d5253a1a79184e3924e86c311bd0c
fjw_Code <1748202178@qq.com>
2018-06-08 13:37:32 +0800

    HTML Parsing

--- utils/html_parsing.py
+++ utils/html_parsing.py
@@ -22,9 +22,41 @@ from os import remove
 from requests_html import HTML
 
 
+p = """# Licensed to the White Turing under one or more
+# contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The SFC licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+'''List of Chromium Command Line Switches.'''
+
+
+class Chromium(object):
+    '''Frequently used commands mappings.
+    
+    There are lots of command lines which can be used with the Google Chrome browser.
+    Some change behavior of features, others are for debugging or experimenting.
+    This page lists the available switches including their conditions and descriptions.
+    Last update occurred on 2018-06-08 from `https://peter.sh/experiments/chromium-command-line-switches/`.
+    '''
+"""
+
+
 def html_parsing(fp=r'utils/commands.py'):
     remove(fp)
-    
+    print(p, file=open(fp, 'a', encoding='utf-8'))
+
     with open(r'html/List of Chromium Command Line Switches « Peter Beverloo.html', encoding='utf-8') as f:
         doc = f.read()
 
@@ -34,10 +66,11 @@ def html_parsing(fp=r'utils/commands.py'):
     
     for i, j in zip(condition, explanation):
         k = i.split('   ')[0].replace("'", '').replace('-', '_').replace('.', '_').upper()
+        j = j.replace('\n', '')
         if len(k) < 1 or not k[0].isalpha():
             continue
-        print(f'{k} = {i.strip()!r}   # {j.strip()}', file=open(fp, 'a', encoding='utf-8'))
+        print(f'    {k} = {i.strip()!r}   # {j.strip()}', file=open(fp, 'a', encoding='utf-8'))
 
 
 if __name__ == '__main__':
-    html_parsing()
+    html_parsing('platinum/chromium.py')
