e4989b190d2d822974b1835f3b719949e398c2ff
fjw_Code <1748202178@qq.com>
2019-01-22 16:40:57 +0800

    Parsing for producing platinum/data/ios.json.

--- utils/html_parsing.py
+++ utils/html_parsing.py
@@ -22,38 +22,38 @@ from os import remove
 from requests_html import HTML
 
 
-p = """# Licensed to the White Turing under one or more
-# contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The SFC licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# Produce platinum/chromium.py
+def html_parsing_chromium(fp=r'utils/commands.py'):
+    p = """# Licensed to the White Turing under one or more
+    # contributor license agreements.  See the NOTICE file
+    # distributed with this work for additional information
+    # regarding copyright ownership.  The SFC licenses this file
+    # to you under the Apache License, Version 2.0 (the
+    # "License"); you may not use this file except in compliance
+    # with the License.  You may obtain a copy of the License at
+    #
+    #   http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing,
+    # software distributed under the License is distributed on an
+    # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    # KIND, either express or implied.  See the License for the
+    # specific language governing permissions and limitations
+    # under the License.
 
-'''List of Chromium Command Line Switches.'''
+    '''List of Chromium Command Line Switches.'''
 
 
-class Chromium(object):
-    '''Frequently used commands mappings.
+    class Chromium(object):
+        '''Frequently used commands mappings.
+        
+        There are lots of command lines which can be used with the Google Chrome browser.
+        Some change behavior of features, others are for debugging or experimenting.
+        This page lists the available switches including their conditions and descriptions.
+        Last update occurred on 2018-06-08 from `https://peter.sh/experiments/chromium-command-line-switches/`.
+        '''
+    """
     
-    There are lots of command lines which can be used with the Google Chrome browser.
-    Some change behavior of features, others are for debugging or experimenting.
-    This page lists the available switches including their conditions and descriptions.
-    Last update occurred on 2018-06-08 from `https://peter.sh/experiments/chromium-command-line-switches/`.
-    '''
-"""
-
-
-def html_parsing(fp=r'utils/commands.py'):
     remove(fp)
     print(p, file=open(fp, 'a', encoding='utf-8'))
 
@@ -72,5 +72,24 @@ def html_parsing(fp=r'utils/commands.py'):
         print(f'    {k} = {i.strip()!r}   # {j.strip()}', file=open(fp, 'a', encoding='utf-8'))
 
 
+# Produce platinum/data/ios.json
+# https://en.wikipedia.org/wiki/IOS_version_history
+def html_parsing_ios(fp=r'utils/ios.json'):
+    import json
+    
+    with open(r'html/iOS version history - Wikipedia.htm', encoding='utf-8') as f:
+        doc = f.read()
+        
+    html = HTML(html=doc)
+    nv = html.xpath('//tr[@valign="top"]/th[not(@colspan)]/text()[1]')[64: -1]
+    cv = html.xpath('//tr[@valign="top"]/td[1]/text()[1]')[64:-1]
+
+    nv = map(lambda x: x.strip(), nv)
+    cv = map(lambda x: x.strip().split('/')[-1], cv)
+    
+    json.dump(dict(zip(nv, cv)), fp=open(fp, 'w'))
+
+
 if __name__ == '__main__':
-    html_parsing('platinum/chromium.py')
+    # html_parsing_chromium('platinum/chromium.py')
+    html_parsing_ios('platinum/data/ios.json')
